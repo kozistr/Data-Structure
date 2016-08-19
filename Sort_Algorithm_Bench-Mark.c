9f3463439c29d768506e37a453274d894dd3bb29
kozistr <kozistr@gmail.com>
2016-08-19 22:06:04 +0900

    Create Sort_Algorithm_Bench-Mark.c

---
+++ Sort_Algorithm_Bench-Mark.c
@@ -0,0 +1,133 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#define MAX 10000
+#define swap(a, b) ((&(a) == &(b))? a : ( a ^= b, b ^=a, a ^= b))
+
+int buf[MAX];
+
+int IsNumExist(int num, int index) {
+	for (int i = 0; i < index; ++i) {
+		if (buf[i] == num)
+			return 1;
+	}
+
+	return 0;
+}
+
+void display(void) {
+	for (int i = 1; i <= MAX; (i % 10 == 0) ? printf("%3d\n", buf[i - 1]) : printf("%3d ", buf[i - 1]), ++i);
+}
+void gen_rand(void) {
+	srand((unsigned)time(0));
+
+	for (int i = 0; i < MAX;) {
+		int num = rand() % MAX + 1;
+
+		if (!IsNumExist(num, i)) {
+			buf[i] = num;
+			++i;
+		}
+	}
+}
+
+void insert_sort(void) {
+	for (int i = 0; i < MAX; ++i) {
+		int tmp = buf[i];
+
+		int j = i;
+		while (buf[j - 1] > tmp && j > 0) {
+			buf[j] = buf[j - 1];
+			--j;
+		}
+
+		buf[j] = tmp;
+	}
+}
+void select_sort(void) {
+	for (int i = 0; i < MAX; ++i) {
+		int min = i;
+
+		for (int j = i; j < MAX; ++j)
+			if (buf[j] < buf[min])
+				min = j;
+
+		swap(buf[min], buf[i]);
+	}
+}
+void bubble_sort(void) {
+	for (int i = MAX - 1; i >= 0; --i) {
+		for (int j = 1; j <= i; ++j) {
+			if (buf[j - 1] > buf[j])
+				swap(buf[j - 1], buf[j]);
+		}
+	}
+}
+void shell_sort(void) {
+	int group;
+	for (group = 0; group < MAX; group = 3 * group + 1);
+	for (; group > 0; group /= 3) {
+		for (int i = group; i < MAX; ++i) {
+			int tmp = buf[i];
+
+			int j = i;
+			while (j >= group && buf[j - group] > tmp) {
+				buf[j] = buf[j - group];
+				j -= group;
+			}
+
+			buf[j] = tmp;
+		}
+	}
+}
+void quick_sort(int *data, int left, int right) {
+	if (right > left) {
+		int num = data[right];
+
+		int i = left - 1;
+		int j = right;
+
+		for (;;) {
+			while (data[++i] < num);
+			while (data[--j] > num);
+			if (i >= j) break;
+
+			swap(data[i], data[j]);
+		}
+
+		swap(data[i], data[right]);
+
+		quick_sort(data, left, i - 1);
+		quick_sort(data, i + 1, right);
+	}
+}
+
+void main(int argc, char *argv[]) {
+	time_t log[5][2];
+	char *name[] = { "Bubble", "Insert", "Select", "Shell", "Quick" };
+	LPVOID func[] = { bubble_sort, insert_sort, select_sort, shell_sort, };
+	
+	printf("- Sorting Algorithm BenchMark\n\n");
+
+	printf("[+] Generating Random Numbers\n");
+
+	printf("\n[+] Sorting\n");
+
+	for (int i = 0; i < 4; ++i) {
+		gen_rand();
+		printf("   [*] %s Sort : \n", name[i]);
+		log[i][0] = clock();
+		FARPROC proc = (FARPROC)func[i]; proc();
+		log[i][1] = clock() - log[i][0];
+		printf("\tElapsed Time : %fs\n", (float)log[i][1] / (CLOCKS_PER_SEC));
+	}
+
+	gen_rand();
+	printf("   [*] Quick Sort : \n");
+	log[4][0] = clock();
+	quick_sort(buf, 0, MAX - 1);
+	log[4][1] = clock() - log[4][0];
+	printf("\tElapsed Time : %fs\n", (float)log[4][1] / (CLOCKS_PER_SEC));
+}
